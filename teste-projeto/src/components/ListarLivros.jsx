6bf6c27efb6570e10b0d506cc4d3616f0b9507ea
Rivanildo Pires <rivanildopires1@gmail.com>
2025-05-15 15:39:28 -0300

    Ajustes no CSS

--- teste-projeto/src/components/ListarLivros.jsx
+++ teste-projeto/src/components/ListarLivros.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/logo.png";
 import livro from "../assets/livro.png";
 import usuario from "../assets/usuario.png";
 import sair from "../assets/sair.png";
-import java from "../assets/java.png"
+import java from "../assets/java.png";
 import { Link } from "react-router-dom";
 
 const ListarLivros = () => {
@@ -18,7 +18,9 @@ const ListarLivros = () => {
         <nav className="nav-bar">
           <div className="navbar-inner">
             <div className="navbar-top">
-              <Link to={"/"}><img src={logo} alt="logo-biblioteca" /></Link>
+              <Link to={"/"}>
+                <img src={logo} alt="logo-biblioteca" />
+              </Link>
 
               <form className="form">
                 <input type="text" placeholder="Buscar..." />
@@ -60,27 +62,44 @@ const ListarLivros = () => {
         </nav>
       </header>
 
-      <aside className="categories">
-        <h4>Livros</h4>
-        <div className="separator">
-          <div className="line"></div>
+      <div className="container">
+        <aside className="categories">
+          <h4>Livros</h4>
+          <div className="separator">
+            <div className="line"></div>
+          </div>
+          <ul>
+            <li>Ciências da Computação</li>
+            <li>Direito</li>
+            <li>Educação Fisica</li>
+            <li>Marketing</li>
+            <li>Matemática</li>
+          </ul>
+        </aside>
+        <div className="container-main">
+          <main className="main-content">
+            <section className="section-livros">
+              <div className="livro">
+                <img src={java} alt="" /> <h5>Designer Patterns</h5>
+              </div>
+              <div className="livro">
+                <img src={java} alt="" /> <h5>Designer Patterns</h5>
+              </div>
+              <div className="livro">
+                <img src={java} alt="" /> <h5>Designer Patterns</h5>
+              </div>
+              <div className="livro">
+                <img src={java} alt="" /> <h5>Designer Patterns</h5>
+              </div>
+              <div className="livro">
+                <img src={java} alt="" /> <h5>Designer Patterns</h5>
+              </div>
+              <div className="livro">
+                <img src={java} alt="" /> <h5>Designer Patterns</h5>
+              </div>
+            </section>
+          </main>
         </div>
-        <ul>
-          <li>Ciências da Computação</li>
-          <li>Direito</li>
-          <li>Educação Fisica</li>
-          <li>Marketing</li>
-          <li>Matemática</li>
-        </ul>
-      </aside>
-      <div className="container-main">
-        <main className="main-content">
-          <section className="section-livros">
-            <div className="livro"><img src={java} alt="" /> <h5>Designer Patterns</h5></div>
-            <div className="livro"><img src={java} alt="" /> <h5>Designer Patterns</h5></div>
-            <div className="livro"><img src={java} alt="" /> <h5>Designer Patterns</h5></div>
-          </section>
-        </main>
       </div>
       <footer>
         <h3>
